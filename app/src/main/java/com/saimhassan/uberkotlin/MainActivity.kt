58facfd6df1c483804e454d373cb9170db77c381
Saim <saimh355@gmail.com>
2020-06-22 23:31:32 +0500

    Uber app using kotlin as a programming language.

---
+++ app/src/main/java/com/saimhassan/uberkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.saimhassan.uberkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
