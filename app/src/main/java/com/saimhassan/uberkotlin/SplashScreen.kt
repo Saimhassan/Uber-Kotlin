c40cfc3d324450845c6251cadbcef2d28739c779
Saim <saimh355@gmail.com>
2020-06-23 09:45:13 +0500

    Add Splash Screen Code in Kotlin File.

--- app/src/main/java/com/saimhassan/uberkotlin/SplashScreen.kt
+++ app/src/main/java/com/saimhassan/uberkotlin/SplashScreen.kt
@@ -2,10 +2,18 @@ package com.saimhassan.uberkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        Completable.timer(3,TimeUnit.SECONDS,AndroidSchedulers.mainThread())
+            .subscribe({
+                Toast.makeText(this@SplashScreen,"Splash Screen Run Done!",Toast.LENGTH_SHORT).show();
+            })
     }
 }
