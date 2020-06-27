e208b87a5143c9935ac4a6c2e834f56bba8f5bc1
Saim <saimh355@gmail.com>
2020-06-27 18:27:51 +0500

    Done with Driver APP Authentication User(Phone & Google)

--- app/src/main/java/com/saimhassan/uberkotlin/SplashScreen.kt
+++ app/src/main/java/com/saimhassan/uberkotlin/SplashScreen.kt
@@ -1,12 +1,17 @@
 package com.saimhassan.uberkotlin
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import io.reactivex.Completable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 class SplashScreen : AppCompatActivity() {
@@ -39,7 +44,54 @@ class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        init()
 
+    }
+
+    private fun init() {
+        providers = Arrays.asList(
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        listener = FirebaseAuth.AuthStateListener { myFirebaseAuth ->
+            val user = myFirebaseAuth.currentUser
+            if (user != null)
+                Toast.makeText(this@SplashScreen,"Welcome"+user.uid,Toast.LENGTH_SHORT).show()
+            else
+                showLoginLayout()
+        }
+    }
+
+    private fun showLoginLayout() {
+        val authMethodPickerLayout = AuthMethodPickerLayout.Builder(R.layout.layout_sign_in)
+            .setPhoneButtonId(R.id.btn_phone_sign_in)
+            .setGoogleButtonId(R.id.btn_email_sign_in)
+            .build();
+       startActivityForResult(
+           AuthUI.getInstance()
+               .createSignInIntentBuilder()
+               .setAuthMethodPickerLayout(authMethodPickerLayout)
+               .setTheme(R.style.LogInTheme)
+               .setAvailableProviders(providers)
+               .setIsSmartLockEnabled(false)
+               .build()
+           ,LOG_IN_REQUEST_CODE
+       )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == LOG_IN_REQUEST_CODE){
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK)
+            {
+                val user = FirebaseAuth.getInstance().currentUser
+            }
+            else
+                Toast.makeText(this@SplashScreen,""+response!!.error!!.message,Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
