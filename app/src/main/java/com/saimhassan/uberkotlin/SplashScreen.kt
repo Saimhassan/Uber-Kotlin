ad63f600e02793a630762dbbcd0b9e15ac0de166
Saim <saimh355@gmail.com>
2020-06-27 18:01:05 +0500

    Create delaySplashScreen Method, onStart() and onStop() methods

--- app/src/main/java/com/saimhassan/uberkotlin/SplashScreen.kt
+++ app/src/main/java/com/saimhassan/uberkotlin/SplashScreen.kt
@@ -3,17 +3,43 @@ package com.saimhassan.uberkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 import io.reactivex.Completable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import java.util.concurrent.TimeUnit
 
 class SplashScreen : AppCompatActivity() {
+
+    companion object{
+        private val LOG_IN_REQUEST_CODE = 7171;
+    }
+
+    private lateinit var providers:List<AuthUI.IdpConfig>
+    private lateinit var firebaseAuth:FirebaseAuth
+    private lateinit var listener:FirebaseAuth.AuthStateListener
+
+    override fun onStart() {
+        super.onStart()
+        delaySplashScreen();
+    }
+
+    override fun onStop() {
+        if (firebaseAuth != null && listener != null) firebaseAuth.removeAuthStateListener(listener)
+        super.onStop()
+    }
+
+    private fun delaySplashScreen() {
+        Completable.timer(3,TimeUnit.SECONDS,AndroidSchedulers.mainThread())
+            .subscribe({
+                firebaseAuth.addAuthStateListener(listener)
+            })
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Completable.timer(3,TimeUnit.SECONDS,AndroidSchedulers.mainThread())
-            .subscribe({
-                Toast.makeText(this@SplashScreen,"Splash Screen Run Done!",Toast.LENGTH_SHORT).show();
-            })
+
     }
 }
